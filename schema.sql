3d1aa92f1bb4e6d06565893d9ae1623f6ddac024
mikiyasassefa <138203126+mikiyasassefa@users.noreply.github.com>
2023-09-29 17:43:50 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -191,3 +191,5 @@ ALTER TABLE `Order` ADD FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
 ALTER TABLE `Order` ADD FOREIGN KEY (`table_id`) REFERENCES `Table` (`table_id`);
 
 ALTER TABLE `Menu_order` ADD FOREIGN KEY (`menu_id`) REFERENCES `Menu` (`menu_id`);
+
+-- miki
