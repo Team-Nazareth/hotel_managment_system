4793074696da906fd8f399453bebde71bd64ac63
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2023-10-17 15:43:17 +0300

    order a menu

---
+++ procedures/orderMenu.sql
@@ -0,0 +1,22 @@
+--orders a menu
+DELIMITER //
+DROP PROCEDURE IF EXISTS order_menu;
+CREATE PROCEDURE order_menu(
+    IN g_id INT,
+    IN t_id INT,
+    IN quant INT,
+    IN mn_id INT
+)
+BEGIN
+    START TRANSACTION;
+          INSERT INTO `order` (guest_id,table_id,quantity) VALUES
+      (g_id,t_id,quant);
+          IF mn_id NOT IN (SELECT menu_id FROM menu) THEN 
+        ROLLBACK;
+          ELSE 
+        INSERT INTO menu_order (menu_id,order_id) VALUES
+        (mn_id,LAST_INSERT_ID());
+        COMMIT;
+          END IF;
+END //
+DELIMITER ;
